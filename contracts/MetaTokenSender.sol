545b4ed28c1b10268df59e4cf353308c1097ba08
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-12-17 10:26:30 -0500

    Meta-Transaction

--- contracts/MetaTokenSender.sol
+++ contracts/MetaTokenSender.sol
@@ -11,3 +11,24 @@ contract RandomToken is ERC20 {
         _mint(msg.sender, amount);
     }
 }
+
+contract TokenSender {
+
+    using ECDSA for bytes32;
+
+    function transfer(address sender, uint amount, address recipient, address tokenContract, bytes memory signature) public {
+        bytes32 messageHash = getHash(sender, amount, recipient, tokenContract);
+        bytes32 signedMessageHash = messageHash.toEthSignedMessageHash();
+
+        address signer = signedMessageHash.recover(signature);
+
+        require(signer == sender, "Signature does not come from sender");
+
+        bool sent = ERC20(tokenContract).transferFrom(sender, recipient, amount);
+        require(sent, "Transfer failed");
+    }
+
+    function getHash(address sender, uint amount, address recipient, address tokenContract) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(sender, amount, recipient, tokenContract));
+    }
+}
