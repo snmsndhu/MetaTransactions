98b1d11d98ac510460c4944d595d89bd83a4b3fb
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-12-17 10:23:34 -0500

    Meta-Transaction

---
+++ contracts/MetaTokenSender.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract RandomToken is ERC20 {
+    constructor() ERC20("", "") {}
+
+    function freeMint(uint amount) public {
+        _mint(msg.sender, amount);
+    }
+}
